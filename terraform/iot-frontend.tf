3f1e6808fd06cec33174fdcc96186dcf38d46011
Johan Svensson <johan@reducer.io>
2023-03-04 14:20:57 +0100

    nodered: get rid of it for now

--- terraform/iot-frontend.tf
+++ terraform/iot-frontend.tf
@@ -4,10 +4,3 @@ resource "kubernetes_namespace_v1" "iot_frontend" {
     name = "iot-frontend"
   }
 }
-
-module "node_red" {
-  source    = "./module/iot-frontend/node-red"
-  namespace = kubernetes_namespace_v1.iot_frontend.metadata.0.name
-
-  ui_host = "nodered.${var.domain}"
-}
