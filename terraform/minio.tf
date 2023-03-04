c43bc3dd1af180c2ad05371359e58e4ebbf62e84
Johan Svensson <johan@reducer.io>
2023-03-04 22:56:44 +0100

    minio: test config of managed Longhorn

--- terraform/minio.tf
+++ terraform/minio.tf
@@ -1,4 +1,11 @@
+#############################################
+###
+### Terraform configuration for Minio
+###
+
 # Create the credentials Terraform uses to access Minio.
+# This user must be manually created in Minio using the credentials in 1Password
+# so that it has access for future operations.
 resource "onepassword_item" "minio_terraform" {
   vault = var.onepassword_vault_id
 
@@ -6,8 +13,63 @@ resource "onepassword_item" "minio_terraform" {
   category = "login"
 
   username = "terraform"
+  url      = "192.168.68.66:9000"
 
   password_recipe {
     length = 50
   }
+
+  section {
+    label = "settings"
+    field {
+      label = "region_name"
+      value = "protea"
+    }
+  }
+}
+
+#############################################
+###
+### Managed Longhorn test
+###
+
+# Test to see if I can manage the Longhorn user from here.
+# Current user was manually created.
+resource "minio_iam_user" "longhorn" {
+  name = "longhorn-managed"
+}
+
+resource "minio_s3_bucket" "longhorn_managed" {
+  bucket = "longhorn-managed"
+  acl    = "private"
+}
+
+# Create policy to allow RW access to Longhorn bucket.
+resource "minio_iam_policy" "longhorn_rw" {
+  name = "longhorn-rw"
+
+  policy = jsonencode(
+    {
+      "Version" = "2012-10-17",
+      "Statement" = [
+        {
+          "Effect" = "Allow",
+          "Action" = [
+            "s3:GetObject",
+            "s3:PutObject",
+            "s3:GetBucketLocation",
+          ],
+          "Resource" = [
+            "arn:aws:s3:::${minio_s3_bucket.longhorn_managed.bucket}"
+          ]
+        }
+      ]
+    }
+  )
+}
+
+# Attach policy to Longhorn user.
+resource "minio_iam_user_policy_attachment" "longhorn_user_rw" {
+  user_name   = minio_iam_user.longhorn.id
+  policy_name = minio_iam_policy.longhorn_rw.id
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,3 @@ variable "onepassword_vault_id" {
   type        = string
   description = "1Password vault ID to use for secret storage."
 }
-
-variable "minio_region" {
-  type        = string
-  description = "The region to use for Minio."
-}
-
-variable "minio_server" {
-  type        = string
-  description = "The Minio host."
-}
