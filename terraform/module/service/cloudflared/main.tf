34576e5ba75cfd1d23f52c482814e950e8c803d4
Johan Svensson <johan@reducer.io>
2023-06-15 23:59:35 +0200

    chore(cloudflared): override image version in chart

--- terraform/module/service/cloudflared/main.tf
+++ terraform/module/service/cloudflared/main.tf
@@ -6,6 +6,12 @@ resource "helm_release" "cloudflared" {
   chart      = "cloudflared"
   version    = "1.0.3"
 
+  # Override cloudflared version in chart
+  set {
+    name  = "image.tag"
+    value = "2023.6.0"
+  }
+
   set {
     name  = "tunnelID"
     value = var.tunnel_id
