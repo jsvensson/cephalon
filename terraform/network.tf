ca90ae5dbc8652c5282a726da9f00983d440bf06
Johan Svensson <johan@reducer.io>
2023-06-15 19:35:25 +0200

    chore(cloudflare): rework tunnel setup

--- terraform/network.tf
+++ terraform/network.tf
@@ -4,7 +4,7 @@ resource "kubernetes_namespace_v1" "network" {
   }
 }
 
-### Cloudflare provider credentials
+### Cloudflare provider credentials. Password is the API token from Cloudflare.
 resource "onepassword_item" "cloudflare_provider" {
   vault = data.onepassword_vault.homelab.uuid
 
@@ -15,43 +15,35 @@ resource "onepassword_item" "cloudflare_provider" {
     label = "Settings"
 
     field {
-      label = "API token"
+      label = "Account ID"
     }
   }
 
 }
 
 ### Cloudflare Tunnel credentials
-# Create 1Password item. Manually populated with the Cloudflare tokens etc.
+# Create 1Password item. Manually populated with the tunnel token.
 resource "onepassword_item" "cloudflare_tunnel" {
   vault = data.onepassword_vault.homelab.uuid
 
   title    = "Cloudflare Tunnel"
   category = "password"
-
-  section {
-    label = "Settings"
-
-    field {
-      label = "Tunnel ID"
-    }
-
-    field {
-      label = "Tunnel name"
-    }
-
-    field {
-      label = "Argo account tag"
-    }
-  }
 }
 
+# Create tunnel
+resource "cloudflare_tunnel" "homelab_tunnel" {
+  name       = "homelab"
+  account_id = onepassword_item.cloudflare_provider.section[0].field[0].value
+  secret     = base64encode(onepassword_item.cloudflare_tunnel.password)
+}
+
+# Start cloudflared with the tunnel config from above
 module "cloudflared" {
   source    = "./module/service/cloudflared"
   namespace = kubernetes_namespace_v1.network.metadata[0].name
 
-  tunnel_id     = onepassword_item.cloudflare_tunnel.section[0].field[0].value
-  tunnel_name   = onepassword_item.cloudflare_tunnel.section[0].field[1].value
-  account_tag   = onepassword_item.cloudflare_tunnel.section[0].field[2].value
-  tunnel_secret = onepassword_item.cloudflare_tunnel.password
+  tunnel_id     = cloudflare_tunnel.homelab_tunnel.id
+  tunnel_name   = cloudflare_tunnel.homelab_tunnel.name
+  account_tag   = onepassword_item.cloudflare_provider.section[0].field[0].value
+  tunnel_secret = cloudflare_tunnel.homelab_tunnel.secret
 }
